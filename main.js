2690fbf6979b860641698364af6489e8de634d7c
NoOneHardy <silas.hardegger@outlook.com>
2023-05-15 11:58:07 +0200

    Navbar

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const urlParams = new URLSearchParams(window.location.search)
 const todo = document.querySelector('#todo')
 
+document.querySelector(':root').style = '--nav-link-glow:' + '#0000ff'
+
 toDoName = urlParams.get('name')
 todo.innerHTML = toDoName
