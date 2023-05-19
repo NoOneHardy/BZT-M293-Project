717a0217ee632c72879dc245c7792f4a74b8206d
NoOneHardy <silas.hardegger@outlook.com>
2023-05-19 10:02:32 +0200

    settings

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const urlParams = new URLSearchParams(window.location.search)
 const todo = document.querySelector('#todo')
 
-document.querySelector(':root').style.setProperty('--primary-color', '#ff0')
-document.querySelector(':root').style.setProperty('--secondary-color', '#f80')
+document.querySelector(':root').style.setProperty('--primary-color', '#0f8')
+document.querySelector(':root').style.setProperty('--secondary-color', '#084')
 
 toDoName = urlParams.get('name')
 todo.innerHTML = toDoName
